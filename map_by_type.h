9b2078750c39a13f4c41d006aee7b152aa613edf
Alan Grover <awgrover@gmail.com>
2022-02-19 17:20:50 -0500

    factor map_by_type.h

---
+++ map_by_type.h
@@ -0,0 +1,14 @@
+// probably factor into "fixes":
+template <class I, class O>
+O map(I x, I in_min, I in_max, O out_min, O out_max){
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+// special case for int and char, to prevent "overflow" math
+template <class I>
+int map(I x, I in_min, I in_max, int out_min, int out_max){
+  return (long)(x - in_min) * (long)(out_max - out_min) / (long)(in_max - in_min) + out_min;
+}
+template <class I>
+int map(I x, I in_min, I in_max, byte out_min, byte out_max){
+  return (int)(x - in_min) * (int)(out_max - out_min) / (int)(in_max - in_min) + out_min;
+}
