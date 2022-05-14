e33a4475faedf6df30180e3c8ce49c89fc0cb7fe
Alan Grover <awgrover@gmail.com>
2022-05-14 10:55:59 -0400

    add i2c_scan.ino

---
+++ i2c_scan.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+
+void i2c_scan() {
+  int nDevices = 0;
+
+  Serial.println("Scanning I2C...");
+
+  for (byte address = 1; address < 127; ++address) {
+    // The i2c_scanner uses the return value of
+    // the Wire.endTransmission to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    byte error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.print(address, HEX);
+      Serial.println("  !");
+
+      ++nDevices;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16) {
+        Serial.print("0");
+      }
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  } else {
+    Serial << "Found " << nDevices << " I2C's" << endl;
+  }
+}
